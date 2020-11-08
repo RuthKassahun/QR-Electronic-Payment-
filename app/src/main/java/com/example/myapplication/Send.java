ef4c5a3aa5e5e5caae503edffdd292ee8b49db4a
Ebenezer <ebenezer2endale@gmail.com>
2020-11-08 13:18:46 +0300

    Scanner and Generator added Splash changed

--- app/src/main/java/com/example/myapplication/Send.java
+++ app/src/main/java/com/example/myapplication/Send.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,9 +12,13 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class Send extends AppCompatActivity {
-
+    private Button scan_btn;
     private LinearLayout viaAccount,viaQR;
     private Button continuebtn,cancelbtn;
     Animation fade;
@@ -21,6 +27,21 @@ public class Send extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
+
+        scan_btn = (Button) findViewById(R.id.scan_btn);
+        final Activity activity = this;
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
@@ -40,8 +61,8 @@ public class Send extends AppCompatActivity {
             public void onClick(View view) {
                 continuebtn.setAlpha(1);
                 cancelbtn.setAlpha(1);
-            continuebtn.startAnimation(fade);
-            cancelbtn.startAnimation(fade);
+                continuebtn.startAnimation(fade);
+                cancelbtn.startAnimation(fade);
 
 
             }
@@ -76,11 +97,24 @@ public class Send extends AppCompatActivity {
         cancelbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               cancelbtn.setAlpha(1);
-               cancelbtn.startAnimation(fade);
+                cancelbtn.setAlpha(1);
+                cancelbtn.startAnimation(fade);
             }
         });
 
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "You Cancelled the Scanning", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else
+            super.onActivityResult(requestCode, resultCode, data);
     }
 }
